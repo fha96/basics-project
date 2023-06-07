96c04b320f1149545c45b45814628ec540393ed4
fha96 <fahadalrimawe96@hotmail.com>
2023-06-07 14:25:21 +0300

    refactor(dropDown): remove consoles

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -9,7 +9,6 @@ export class DropDownDirective {
     
     @HostBinding('class.open') isOpen = false;
     @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
-      console.log(event.target);
       this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
     constructor(private elRef: ElementRef) {
