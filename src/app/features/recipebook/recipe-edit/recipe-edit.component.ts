8e4e6840e6dde42a158c2a9993fb3c2bf7b8337d
fha96 <fahadalrimawe96@hotmail.com>
2023-07-13 14:48:14 +0300

    add validators v1

--- src/app/features/recipebook/recipe-edit/recipe-edit.component.ts
+++ src/app/features/recipebook/recipe-edit/recipe-edit.component.ts
@@ -1,68 +1,76 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { RecipeService } from '../recipeservice.service';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
-  styleUrls: ['./recipe-edit.component.css']
+  styleUrls: ['./recipe-edit.component.css'],
 })
-export class RecipeEditComponent implements OnInit{
-  id: number ;
-  editMode: boolean = false ;
-  recipeEditForm: FormGroup ;
-  constructor(private activatedRoute: ActivatedRoute,
-              private recipeService: RecipeService){}
+export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode: boolean = false;
+  recipeEditForm: FormGroup;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(
-      (params: Params) => {
-        this.id = +params.id ;
-        this.editMode = params.id != null ;
-        console.log(this.editMode);
-        this.initForm();
-      }
-    );
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.id = +params.id;
+      this.editMode = params.id != null;
+      console.log(this.editMode);
+      this.initForm();
+    });
   }
 
-
   private initForm() {
-    let recipeName = '' ;
+    let recipeName = '';
     let recipeImage = '';
-    let recipeDescription = '' ;
+    let recipeDescription = '';
     let recipeIngredients = new FormArray([]);
 
-    if(this.editMode) {
+    if (this.editMode) {
       const recipe = this.recipeService.getRecipeById(this.id);
-      recipeName =  recipe.name ;
-      recipeImage = recipe.imagePath ;
-      recipeDescription = recipe.description ;
-      if(recipe.ingredients){
-        for( let ingredient of recipe.ingredients){
+      recipeName = recipe.name;
+      recipeImage = recipe.imagePath;
+      recipeDescription = recipe.description;
+      if (recipe.ingredients) {
+        for (let ingredient of recipe.ingredients) {
           recipeIngredients.push(
             new FormGroup({
-              name: new FormControl(ingredient.name),
-              amount: new FormControl(ingredient.amount)
+              name: new FormControl(ingredient.name, Validators.required),
+              amount: new FormControl(ingredient.amount, [Validators.required, Validators.pattern()]),
             })
           );
         }
       }
     }
     this.recipeEditForm = new FormGroup({
-      name: new FormControl(recipeName),
-      imagepath: new FormControl(recipeImage),
-      description: new FormControl(recipeDescription),
-      ingredients: recipeIngredients
+      name: new FormControl(recipeName, Validators.required),
+      imagepath: new FormControl(recipeImage, Validators.required),
+      description: new FormControl(recipeDescription,  Validators.required),
+      ingredients: recipeIngredients,
     });
   }
-  onSubmit(){
+  onSubmit() {
     console.log(this.recipeEditForm);
-    
   }
 
-
-  getIngredientsControls(){
+  getIngredientsControls() {
     return (<FormArray>this.recipeEditForm.get('ingredients')).controls;
+  }
+
+  onAddIngredient(){
+    console.log('hHELESKSDK');
+    
+    (<FormArray>this.recipeEditForm.get('ingredients')).push(
+      new FormGroup({
+        name: new FormControl(),
+        amount: new FormControl()
+      })
+    )
   }
 }
